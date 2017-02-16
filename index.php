a9b7c77e3bdadec88dea510fd1134318485387cb
mostmark <martin@ostmarks.se>
2017-02-16 19:27:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  echo "I'm version 1";
+  echo "I'm version 2";
   echo "<br><br>The time is " . date("h:i:sa");
   echo "<br><br>The hostname is " . gethostname();
   echo "<br><br>My Pod IP is : ".$_SERVER['SERVER_ADDR'];
